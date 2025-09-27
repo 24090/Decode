0286ef1cd7ce2417f5c2a9918f5c8fac98eae24c
hmoose-ash <142426250+hmoose-ash@users.noreply.github.com>
2025-09-27 18:51:56 +0200

    Update Controlled.kt
    move while shoot?????

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Controlled.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Controlled.kt
@@ -2,15 +2,17 @@ package org.firstinspires.ftc.teamcode.opmodes.teleop
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
 import org.firstinspires.ftc.teamcode.commands.*
 import org.firstinspires.ftc.teamcode.subsystems.drive.Drive
 import org.firstinspires.ftc.teamcode.drivetrain.Pose
 import org.firstinspires.ftc.teamcode.drivetrain.Vector
-import org.firstinspires.ftc.teamcode.opmodes.poses.farPose
 import org.firstinspires.ftc.teamcode.opmodes.poses.scorePosition
+import org.firstinspires.ftc.teamcode.subsystems.controlsystems.SquID
 import org.firstinspires.ftc.teamcode.subsystems.intake.Intake
 import org.firstinspires.ftc.teamcode.subsystems.shooter.Shooter
 import org.firstinspires.ftc.teamcode.util.BulkReads
+import kotlin.math.PI
 
 @TeleOp(name="Controlled")
 class Controlled: LinearOpMode() {
@@ -20,14 +22,14 @@ class Controlled: LinearOpMode() {
         val shooter = Shooter(hardwareMap)
         val intake = Intake(hardwareMap)
         val driveFunction = {
-            drive.strafe = gamepad1.left_stick_x.toDouble();
+            drive.strafe = -gamepad1.left_stick_x.toDouble();
             drive.drive = -gamepad1.left_stick_y.toDouble();
             drive.turn = -gamepad1.right_stick_x.toDouble();
             drive.setMotorPowers()
             drive.localizer.update()
         }
         waitForStart()
-        drive.localizer.pose = Pose(12.0, 12.0, 0.0)
+        drive.localizer.pose = Pose(12.0, 12.0, PI)
         while (opModeIsActive()){
             bulkReads.update()
             driveFunction()
@@ -49,22 +51,29 @@ class Controlled: LinearOpMode() {
                 runBlocking(Race(
                     Forever {
                         bulkReads.update()
-                        drive.update()
-                        shooter.update()
+                        drive.localizer.update()
+
                         intake.update()
+
+                        val relativePose = (Vector.fromPose(drive.localizer.pose) - scorePosition)
+
+                        shooter.setTargetVelocityFromDistance(relativePose.length)
+                        shooter.update()
+
+                        drive.targetPose = Pose(
+                            drive.localizer.x, drive.localizer.y,
+                            relativePose.angle
+                        )
+                        drive.strafe = -gamepad1.left_stick_x.toDouble();
+                        drive.drive = -gamepad1.left_stick_y.toDouble();
+                        drive.turn = SquID(AngleUnit.normalizeRadians(drive.error.heading), Drive.DriveConstants.kSqH)
+                        drive.setMotorPowers()
+
                         telemetry.addData("Shooter velocity", shooter.currentVelocity)
                         telemetry.update()
                     },
                     Sequence(
-                        Instant {
-                            val relativePose = (Vector.fromPose(drive.localizer.pose) - scorePosition)
-                            shooter.setTargetVelocityFromDistance(relativePose.length)
-                            drive.targetPose = Pose(
-                                drive.localizer.x, drive.localizer.y,
-                                relativePose.angle
-                        )
-                        },
-                        WaitUntil { drive.atTargetCircle(0.4, 0.04) },
+                        WaitUntil { drive.error.heading < 0.04 },
                         shooter.waitForVelocity(),
                         intake.releaseBall()
                     )
